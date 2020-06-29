8ff1adfac68cbc7de46c7630b0608a29574a70d4
Eduardo Alves <oo_eduardo@hotmail.com>
2020-06-29 19:48:54 -0300

    Projeto iniciado, mostrando quartos ocupados

---
+++ Ex71Cap6/Rooms.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex71Cap6
+{
+    class Rooms
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+
+    }
+}
